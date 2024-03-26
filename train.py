55a929af0a63892d858e760bb322ff3cdbe5c9b1
kalok Yau <yaukalokevin@gmail.com>
2024-03-26 16:01:24 +0800

    mulGPU1

--- train.py
+++ train.py
@@ -50,8 +50,10 @@ if __name__ == "__main__":
 
     dataset_opts = opts['dataset']
     train_dataset = get_dataset(**dataset_opts)
+    #train_loader = DataLoader(train_dataset,
+    #   batch_size=opts["batch_size"], num_workers=opts['num_workers'], shuffle=True)
     train_loader = DataLoader(train_dataset,
-        batch_size=opts["batch_size"], num_workers=opts['num_workers'], shuffle=True)
+            batch_size=4, num_workers=4, shuffle=True)
     train_loader = add_post(train_loader, get_image)
 
     # Get checkpoint
@@ -64,17 +66,22 @@ if __name__ == "__main__":
 
     # Get model
     model = ADNTrain(opts['learn'], opts['loss'], **opts['model'])
-    model = torch.nn.DataParallel(model) #multiple gpu
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
+    model = torch.nn.DataParallel(model,device_ids=[0,1],output_device=[1])
+    #model = torch.nn.DataParallel(model,device_ids=[0,1]) #multiple gpu
     if opts['use_gpu']: model.cuda()  #用Gpu
     #if opts['use_gpu']: model.cpu()    #用cpu
 #    if path.isfile(checkpoint): model.resume(checkpoint) #my 调用会出问题
-
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # Get logger
     logger = Logger(run_dir, start_epoch, args.run_name)
-    logger.add_loss_log(model.get_loss, opts["print_step"], opts['window_size'])
-    logger.add_iter_visual_log(model.get_visuals, opts['visualize_step'], "train_visuals")
-    logger.add_save_log(model.save, opts['save_step'])
-
+    #logger.add_loss_log(model.get_loss, opts["print_step"], opts['window_size'])
+    #logger.add_iter_visual_log(model.get_visuals, opts['visualize_step'], "train_visuals")
+    #logger.add_save_log(model.save, opts['save_step'])
+    logger.add_loss_log(model.module.get_loss, opts["print_step"], opts['window_size'])
+    logger.add_iter_visual_log(model.module.get_visuals, opts['visualize_step'], "train_visuals")
+    logger.add_save_log(model.module.save, opts['save_step'])
     # Train the model
     for epoch in range(start_epoch, opts['num_epochs']):
         #
@@ -88,8 +95,10 @@ if __name__ == "__main__":
         #以下为原本代码
         #
         for data in logger(train_loader):
-            model.optimize(*data)
-            model.update_lr()
+            #model.optimize(*data)
+            #model.update_lr()
+            model.module.optimize(*data)
+            model.module.update_lr()
         # import torch, gc
         #
         # gc.collect()
